1f703951c0c8a1ae8ce49c0f90d80fa85e7c3a49
Joao Bortolosso <joao.bortolosso@edglobo.com.br>
2022-05-16 18:51:15 -0300

    Corrigindo uri

--- apipython/urls.py
+++ apipython/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('get_quotes_coins', views.request_data_comply, name='data'),
+    path('', views.request_data_comply, name='data'),
 ]
